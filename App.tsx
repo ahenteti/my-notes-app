18f4e879bc4b02f1e6a8570f0890b7ca6383a0e3
ahenteti <ahmad.henteti@gmail.com>
2021-06-02 16:41:55 +0200

    introduce HomeScreen component

--- App.tsx
+++ App.tsx
@@ -1,82 +1,29 @@
 import React, { useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
-import AddMemoryButton from './src/components/AddMemoryButton';
-import Header from './src/components/Header';
-import Memories from './src/components/Memories';
+import HomeScreen from './src/components/screens/HomeScreen';
 import { ThemeContext } from './src/services/ThemeContext';
 import { ThemeEnum } from './src/services/ThemeEnum';
 import { ThemeStorage } from './src/services/ThemeStorage';
 
-const DATA = [
-  {
-    id: '1',
-    label: 'Rzfal',
-    value: 'EW1gd1%&Vo7a5<f628',
-  },
-  {
-    id: '2',
-    label: 'WlfEC',
-    value: '7mBde7K4(55@5NnRgB4#-',
-  },
-  {
-    id: '3',
-    label: 'WBF',
-    value: 'Z$r5)+@6P34kZDd65I5ED',
-  },
-  {
-    id: '10',
-    label: 'Rzfal',
-    value: 'EW1gd1%&Vo7a5<f628',
-  },
-  {
-    id: '20',
-    label: 'WlfEC',
-    value: '7mBde7K4(55@5NnRgB4#-',
-  },
-  {
-    id: '30',
-    label: 'WBF',
-    value: 'Z$r5)+@6P34kZDd65I5ED',
-  },
-  {
-    id: '100',
-    label: 'Rzfal',
-    value: 'EW1gd1%&Vo7a5<f628',
-  },
-  {
-    id: '200',
-    label: 'WlfEC',
-    value: '7mBde7K4(55@5NnRgB4#-',
-  },
-  {
-    id: '300',
-    label: 'WBF',
-    value: 'Z$r5)+@6P34kZDd65I5ED',
-  },
-];
+interface AppProps {
+  themeStorage?: ThemeStorage;
+}
 
-export default function App() {
-  const { themeStorage } = App.dependencies;
+export default function App({ themeStorage = ThemeStorage.getInstance() }: AppProps) {
   const [theme, setTheme] = React.useState(ThemeEnum.Light);
 
-  useEffect(setThemeStateFromLocalStorage, []);
+  useEffect(updateThemeStateFromLocalStorage, []);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <View>
         <StatusBar barStyle='default' />
-        <Header></Header>
-        <Memories memories={DATA}></Memories>
-        <AddMemoryButton></AddMemoryButton>
+        <HomeScreen></HomeScreen>
       </View>
     </ThemeContext.Provider>
   );
 
-  function setThemeStateFromLocalStorage() {
+  function updateThemeStateFromLocalStorage() {
     themeStorage.isDark().then((isDark) => setTheme(isDark ? ThemeEnum.Dark : ThemeEnum.Light));
   }
 }
-
-App.dependencies = {
-  themeStorage: ThemeStorage.getInstance(),
-};
---
+++ src/components/screens/HomeScreen.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { View } from 'react-native';
+import AddMemoryButton from '../AddMemoryButton';
+import Header from '../Header';
+import Memories from '../Memories';
+
+const DATA = [
+  {
+    id: '1',
+    label: 'Rzfal',
+    value: 'EW1gd1%&Vo7a5<f628',
+  },
+  {
+    id: '2',
+    label: 'WlfEC',
+    value: '7mBde7K4(55@5NnRgB4#-',
+  },
+  {
+    id: '3',
+    label: 'WBF',
+    value: 'Z$r5)+@6P34kZDd65I5ED',
+  },
+  {
+    id: '10',
+    label: 'Rzfal',
+    value: 'EW1gd1%&Vo7a5<f628',
+  },
+  {
+    id: '20',
+    label: 'WlfEC',
+    value: '7mBde7K4(55@5NnRgB4#-',
+  },
+  {
+    id: '30',
+    label: 'WBF',
+    value: 'Z$r5)+@6P34kZDd65I5ED',
+  },
+  {
+    id: '100',
+    label: 'Rzfal',
+    value: 'EW1gd1%&Vo7a5<f628',
+  },
+  {
+    id: '200',
+    label: 'WlfEC',
+    value: '7mBde7K4(55@5NnRgB4#-',
+  },
+  {
+    id: '300',
+    label: 'WBF',
+    value: 'Z$r5)+@6P34kZDd65I5ED',
+  },
+];
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Header></Header>
+      <Memories memories={DATA}></Memories>
+      <AddMemoryButton></AddMemoryButton>
+    </View>
+  );
+}
