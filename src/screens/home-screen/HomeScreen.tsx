d82ae1ebbcbd6d5277bfc2706459895c558f00ac
ahenteti <ahmad.henteti@gmail.com>
2021-06-04 16:40:00 +0200

    refactoring - code cleanup

--- src/screens/home-screen/HomeScreen.tsx
+++ src/screens/home-screen/HomeScreen.tsx
@@ -2,58 +2,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { StatusBar, View } from 'react-native';
 import { ADD_MEMORY_SCREEN_NAME, HEADER_HEIGHT, HOME_SCREEN_NAME } from '../../common/Constants';
-import { MemoryStorage } from '../../common/services/MemoryStorage';
 import AddMemoryButton from './AddMemoryButton';
 import HomeScreenHeader from './HomeScreenHeader';
 import Memories from './Memories';
-
-const DATA = [
-  {
-    id: '1',
-    label: 'Rzfal',
-    value: 'EW1gd1%&Vo7a5<f628',
-  },
-  {
-    id: '2',
-    label: 'WlfEC',
-    value: '7mBde7K4(55@5NnRgB4#-',
-  },
-  {
-    id: '3',
-    label: 'WBF',
-    value: 'Z$r5)+@6P34kZDd65I5ED',
-  },
-  {
-    id: '10',
-    label: 'Rzfal',
-    value: 'EW1gd1%&Vo7a5<f628',
-  },
-  {
-    id: '20',
-    label: 'WlfEC',
-    value: '7mBde7K4(55@5NnRgB4#-',
-  },
-  {
-    id: '30',
-    label: 'WBF',
-    value: 'Z$r5)+@6P34kZDd65I5ED',
-  },
-  {
-    id: '100',
-    label: 'Rzfal',
-    value: 'EW1gd1%&Vo7a5<f628',
-  },
-  {
-    id: '200',
-    label: 'WlfEC',
-    value: '7mBde7K4(55@5NnRgB4#-',
-  },
-  {
-    id: '300',
-    label: 'WBF',
-    value: 'Z$r5)+@6P34kZDd65I5ED',
-  },
-];
 
 function HomeScreenComponent({ navigation }: any) {
   return (
